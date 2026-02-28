77763117a565edc439c13f046b3afdd3a05a0320
intergrav <devin@devins.page>
2026-02-28 18:33:02 -0500

    chore: install packages for redesign
    copied over from my personal website

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,14 +4,18 @@ import sitemap from "@astrojs/sitemap";
 import mdx from "@astrojs/mdx";
 import pagefind from "astro-pagefind";
 
+import expressiveCode from "astro-expressive-code";
+
+import icon from "astro-icon";
+
 export default defineConfig({
-	site: "https://skywardmc.org",
-	integrations: [sitemap(), mdx(), pagefind()],
-	redirects: {
-		"/project/adrenaline": "/adrenaline",
-		"/project/additive": "/additive",
-	},
-	build: {
-		target: "static",
-	},
+    site: "https://skywardmc.org",
+    integrations: [sitemap(), mdx(), pagefind(), expressiveCode(), icon()],
+    redirects: {
+        "/project/adrenaline": "/adrenaline",
+        "/project/additive": "/additive",
+    },
+    build: {
+        target: "static",
+    },
 });
