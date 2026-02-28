64f281a659a29bac2c3eaa8215f61b799aea35b6
intergrav <devin@devins.page>
2026-02-28 18:50:35 -0500

    chore: remove all styles
    so that i can start the site redesign. i'll keep the dcadditive, dcadrenaline, etc and modify those later

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,14 @@
 import { defineConfig } from "astro/config";
 
 import sitemap from "@astrojs/sitemap";
+import expressiveCode from "astro-expressive-code";
+import icon from "astro-icon";
 import mdx from "@astrojs/mdx";
 import pagefind from "astro-pagefind";
 
-import expressiveCode from "astro-expressive-code";
-
-import icon from "astro-icon";
-
 export default defineConfig({
 	site: "https://skywardmc.org",
-	integrations: [sitemap(), mdx(), pagefind(), expressiveCode(), icon()],
+	integrations: [sitemap(), expressiveCode(), icon(), mdx(), pagefind()],
 	redirects: {
 		"/project/adrenaline": "/adrenaline",
 		"/project/additive": "/additive",
